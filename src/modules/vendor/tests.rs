d050fd2d983cdfb1b4dec5cf3825763139feb294
rustmailer <pollybase@zohomail.com>
2025-08-28 15:31:00 +0800

    update

--- src/modules/vendor/tests.rs
+++ src/modules/vendor/tests.rs
@@ -10,7 +10,7 @@ use crate::rustmailer_version;
 
 #[tokio::test]
 async fn test1() {
-    let access_token = "ya29.A0AS3H6Nx8aJrWEN2JG6zsSmhp_wg9BSS6i0uPoEbgPsdfdPtEydMK25ne2P0mL7FVu6g_F4rpTIDZxi4CO96LtlWokbTPd69vaVHx07qJfnRchq3lrFBLpm-OPqV0zLXbJ6X9VSsVW0Cd5W5ykSYjkQMXUA4u7iM7bpjps49VQo10_-y4l_FO7L28-Q0HGRCvdXJNRqgaaCgYKAVsSARUSFQHGX2Mi9nJ8LfBUMLimPBr_wEahxg0207";
+    let access_token = "xxx";
     let url = "https://gmail.googleapis.com/gmail/v1/users/me/labels";
     let url = "https://gmail.googleapis.com/gmail/v1/users/me/labels/Label_6886728075529239043";
     let url = "https://gmail.googleapis.com/gmail/v1/users/me/messages?labelIds=INBOX&maxResults=10&pageToken=08792416985640480557";
